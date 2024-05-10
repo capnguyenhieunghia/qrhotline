634d192511e9f35e52308e916470859ca7fb6fb2
Cáp Nguyễn Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-05-10 22:50:31 +0700

    up

--- main.js
+++ main.js
@@ -14,3 +14,41 @@ function updateClock() {
   }
   
   setInterval(updateClock, 1000);
+  // day y
+  window.onload = function() {
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+  
+    var dateElement = document.getElementById("date");
+    dateElement.innerHTML = "Ngày " + day + " tháng " + month + " năm " + year;
+  };
+      // JavaScript để thực hiện hiệu ứng trượt từ dưới lên và hiển thị nội dung trang web
+    
+    // Hiệu ứng trượt từ dưới lên và hiển thị nội dung
+    function showContent() {
+      var loader = document.getElementById('loader');
+      var content = document.getElementById('content');
+      
+      // Hiệu ứng trượt từ dưới lên
+      loader.style.display = 'none';
+      content.style.display = 'block';
+      content.style.transform = 'translateY(100%)';
+      content.style.transition = 'transform 0.5s ease-in';
+      setTimeout(function() {
+        content.style.transform = 'translateY(0)';
+      }, 100);
+      
+      // Tắt hiệu ứng và hiển thị nội dung trang web
+      setTimeout(function() {
+        content.style.transition = '';
+      }, 600);
+    }
+    
+    // Tự động bật hiệu ứng khi tải trang
+    window.addEventListener('load', function() {
+      setTimeout(function() {
+        showContent();
+      }, 2000);
+    });
