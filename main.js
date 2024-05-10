0706c97d5a7ee4141c339f34cf577bc2c8589bd3
Cáp Nguyễn Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-05-10 19:35:55 +0700

    up

---
+++ main.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+  
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+  
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }
+  
+  setInterval(updateClock, 1000);
