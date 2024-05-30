ee3f21f540fd2eb202bf83660aca2e1b5439398d
nghia <capnguyenhieunghia@gmail.com>
2024-05-30 16:51:18 +0700

    up

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-function updateClock() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-  
-    hours = (hours < 10 ? "0" : "") + hours;
-    minutes = (minutes < 10 ? "0" : "") + minutes;
-    seconds = (seconds < 10 ? "0" : "") + seconds;
-  
-    document.getElementById("hours").textContent = hours;
-    document.getElementById("minutes").textContent = minutes;
-    document.getElementById("seconds").textContent = seconds;
-  }
-  
-  setInterval(updateClock, 1000);
+
   window.addEventListener('load', function () {
     var welcomeMessage = document.getElementById('welcome-message');
     var closeButton = document.getElementById('close-button');
