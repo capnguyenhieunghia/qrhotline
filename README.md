906a6b5a3e3078b13be4b96f20e00f9d7a0b0b25
nghia <capnguyenhieunghia@gmail.com>
2024-06-05 18:44:34 +0700

    ip

--- README.md
+++
@@ -1,6 +0,0 @@
-<div class="text-align: center;"><h1>QR_Hotline Tư Vấn Học Sinh Địa Bàn HCM Và Các Tỉnh</h1>
-<p>Admin</p>
-<p>Mỹ Duyên</p>
-<p>Bích Lê</p>
-<p>Hiếu Nghĩa</p>
-</div>
--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
-  window.addEventListener('load', function () {
-    var welcomeMessage = document.getElementById('welcome-message');
-    var closeButton = document.getElementById('close-button');
+window.addEventListener('load', function () {
+  var welcomeMessage = document.getElementById('welcome-message');
+  var closeButton = document.getElementById('close-button');
 
-    // Show the welcome message
-    welcomeMessage.style.display = 'block';
+  // Show the welcome message
+  welcomeMessage.style.display = 'block';
 
-    // Add a click event listener to the close button
-    closeButton.addEventListener('click', function () {
-        // Hide the welcome message
-        welcomeMessage.style.display = 'none';
-    });
+  // Add a click event listener to the close button
+  closeButton.addEventListener('click', function () {
+      // Hide the welcome message
+      welcomeMessage.style.display = 'none';
+  });
 });
 
 // sao chép nội dung
@@ -20,20 +20,20 @@ const ipElements = document.querySelectorAll('.ip');
 
 // Thêm sự kiện click cho các phần tử có class "mk"
 mkElements.forEach(element => {
-  element.addEventListener('click', () => {
-    // Lấy nội dung của phần tử và copy vào clipboard
-    const content = element.querySelector('p').textContent;
-    navigator.clipboard.writeText(content);
-    alert('Đã sao chép nội dung mật khẩu!');
-  });
+element.addEventListener('click', () => {
+  // Lấy nội dung của phần tử và copy vào clipboard
+  const content = element.querySelector('p').textContent;
+  navigator.clipboard.writeText(content);
+  alert('Đã sao chép nội dung mật khẩu!');
+});
 });
 
 // Thêm sự kiện click cho các phần tử có class "ip"
 ipElements.forEach(element => {
-  element.addEventListener('click', () => {
-    // Lấy nội dung của phần tử và copy vào clipboard
-    const content = element.querySelector('p').textContent;
-    navigator.clipboard.writeText(content);
-    alert('Đã sao chép nội dung IP Server!');
-  });
+element.addEventListener('click', () => {
+  // Lấy nội dung của phần tử và copy vào clipboard
+  const content = element.querySelector('p').textContent;
+  navigator.clipboard.writeText(content);
+  alert('Đã sao chép nội dung IP Server!');
+});
 });
