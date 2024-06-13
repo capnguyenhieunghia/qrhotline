00dba8d6a164c628623a51f7402ed0d127b11375
nghia <capnguyenhieunghia@gmail.com>
2024-06-13 15:20:48 +0700

    up

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ fetch('https://docs.google.com/spreadsheets/d/1wmVxlgrp4gHr-0y6RxY-_t_oZh5aFgBD2
           <h3>${tenMayNhanh}</h3>
           <p>Mật khẩu: ${matKhau} <span class="copy-btn" onclick="copyToClipboard('${matKhau}')">Copy</span></p>
           <p>IP server: ${ipServer} <span class="copy-btn" onclick="copyToClipboard('${ipServer}')">Copy</span></p>
-          <p id="tt" class="${statusClass}">Trạng thái: ${trangThai}</p>
+          <p id="tt" class="${statusClass}"> ${trangThai}</p>
         `;
         cardContainer.appendChild(card);
       }
